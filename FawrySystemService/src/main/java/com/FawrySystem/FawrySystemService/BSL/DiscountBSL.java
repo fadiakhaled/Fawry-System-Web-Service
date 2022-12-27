a605cce4b7662bd3c7f7e936e012a4b0ee9727c2
fadiakhaled <110059717+fadiakhaled@users.noreply.github.com>
2022-12-27 23:13:28 +0200

    updated create overall discount API
    Separated codes from Discount controller to discount BSL

--- FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/BSL/DiscountBSL.java
+++ FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/BSL/DiscountBSL.java
@@ -1,12 +1,26 @@
 package com.FawrySystem.FawrySystemService.BSL;
 
-import com.FawrySystem.FawrySystemService.models.Services.Services;
+import com.FawrySystem.FawrySystemService.models.Services.*;
 import com.FawrySystem.FawrySystemService.models.discounts.OverallDiscount;
 
+import java.util.HashMap;
 import java.util.Vector;
 
 public class DiscountBSL {
 
+
+    public HashMap<String, Double> getAllDiscounts() {
+        Services internetService = new InternetService();
+        Services mobileRecharge = new MobileRecharge();
+        Services donations = new Donations();
+        Services landline = new Landline();
+        HashMap<String, Double> services = new HashMap<>();
+        services.put("Internet" , internetService.getDiscount()*100);
+        services.put("Mobile" , mobileRecharge.getDiscount()*100);
+        services.put("Donations" , donations.getDiscount()*100);
+        services.put("Landline" , landline.getDiscount()*100);
+        return services;
+    }
     public boolean setOverallDiscount(double amount) {
         OverallDiscount overallDiscount = new OverallDiscount();
         Vector<Services> services = overallDiscount.getServices();
@@ -16,11 +30,14 @@ public class DiscountBSL {
                 valid = false;
             }
         }
-
         if (valid) {
             overallDiscount.setDiscount(amount);
             return true;
         }
+        return false;
+    }
+
+    public boolean setSpecificDiscount() {
         return false;
     }
 }
--- FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/controllers/DiscountController.java
+++ FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/controllers/DiscountController.java
@@ -1,16 +1,12 @@
 package com.FawrySystem.FawrySystemService.controllers;
 
 import com.FawrySystem.FawrySystemService.BSL.DiscountBSL;
-import com.FawrySystem.FawrySystemService.models.Services.*;
-import com.FawrySystem.FawrySystemService.models.discounts.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 
 @Component
 @RestController
@@ -21,23 +17,13 @@ public class DiscountController {
 
     @PostMapping(value = "/addOverallDiscount")
     public ResponseEntity<Object> createOverallDiscount(@RequestBody Map<String, Double> discAmount) {
-         Services internetService = new InternetService();
-         Services mobileRecharge = new MobileRecharge();
-         Services donations = new Donations();
-         Services landline = new Landline();
-
+        
         Double discount = discAmount.get("amount");
-
         if (AdminController.currentAdmin == null)
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         else {
             if (discountBSL.setOverallDiscount(discount)) {
-                HashMap<String, Double> services = new HashMap<>();
-                services.put("Internet payment discount" , internetService.getDiscount()*100);
-                services.put("Mobile recharge discount" , mobileRecharge.getDiscount()*100);
-                services.put("Donations' discount" , donations.getDiscount()*100);
-                services.put("Landline discount" , landline.getDiscount()*100);
-                return new ResponseEntity<>(services, HttpStatus.OK);
+                return new ResponseEntity<>(discountBSL.getAllDiscounts(), HttpStatus.OK);
             } else {
                 return new ResponseEntity<>("Cannot apply discount", HttpStatus.BAD_REQUEST);
             }
