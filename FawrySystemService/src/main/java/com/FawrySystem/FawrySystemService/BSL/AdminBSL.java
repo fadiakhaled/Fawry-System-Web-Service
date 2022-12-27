727cf321eb572d098e6472adc810fa409a94a62f
fadiakhaled <110059717+fadiakhaled@users.noreply.github.com>
2022-12-28 01:52:34 +0200

    Update AdminBSL.java

--- FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/BSL/AdminBSL.java
+++ FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/BSL/AdminBSL.java
@@ -1,28 +1,18 @@
 package com.FawrySystem.FawrySystemService.BSL;
 
 import com.FawrySystem.FawrySystemService.models.Users.Admin;
+import com.FawrySystem.FawrySystemService.repositories.AdminRepository;
+import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-
+@Component
 public class AdminBSL {
-    private static HashMap<String, Admin> adminEmails = new HashMap<>();
+    private static AdminRepository adminRepository = new AdminRepository();
 
-    public AdminBSL() {
-        Admin initialAdmin = new Admin("maimostafa", "maimostafa@gmail.com", "123");
-        adminEmails.put(initialAdmin.getEmail(), initialAdmin);
-    }
     static public Admin getAdminByEmail(String  email) {
-        if (adminEmails.containsKey(email)) {
-            return adminEmails.get(email);
-        }
-        return null;
+        return adminRepository.getAdminByEmail(email);
     }
 
     public static boolean addAdmin(Admin admin) {
-        if (adminEmails.containsKey(admin.getEmail())) {
-            return false;
-        }
-        adminEmails.put(admin.getEmail(), admin);
-        return true;
+        return adminRepository.addAdmin(admin);
     }
 }
