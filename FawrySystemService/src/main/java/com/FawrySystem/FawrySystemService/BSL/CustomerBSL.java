1f31adb3fcde95ae7ebbf54a1d2b9b142af0d32d
fadiakhaled <110059717+fadiakhaled@users.noreply.github.com>
2022-12-28 01:48:18 +0200

    Added Customer Repositry

--- FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/BSL/CustomerBSL.java
+++ FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/BSL/CustomerBSL.java
@@ -1,47 +1,30 @@
 package com.FawrySystem.FawrySystemService.BSL;
 
 import com.FawrySystem.FawrySystemService.models.Users.Customer;
+import com.FawrySystem.FawrySystemService.repository.CustomersRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 
+@Component
 public class CustomerBSL {
 
-    private static HashMap<String, Customer> customersEmails = new HashMap<>();
-    private static HashMap<String, Customer> customersUsernames = new HashMap<>();
-
-    public CustomerBSL() {
-        Customer initialCustomer = new Customer("habibayasser", "habibayasser@gmail.com", "123");
-
-        customersEmails.put(initialCustomer.getEmail(), initialCustomer);
-        customersUsernames.put(initialCustomer.getUsername(), initialCustomer);
-
-    }
+    private static CustomersRepository customersRepository = new CustomersRepository();
 
     static public Boolean addCustomer(Customer customer) {
-        if (customersEmails.containsKey(customer.getEmail())) {
+        if (customersRepository.checkCustomerExistence(customer.getUsername(), customer.getEmail()))
             return false;
-        }
-        if (customersUsernames.containsKey(customer.getUsername())) {
-            return false;
-        }
-        customersEmails.put(customer.getEmail(), customer);
-        customersUsernames.put(customer.getUsername(), customer);
+
+        customersRepository.addCustomer(customer);
         return true;
     }
 
     static public Customer getCustomerByEmail(String  email) {
-        if (customersEmails.containsKey(email)) {
-            return customersEmails.get(email);
-        }
-        return null;
+        return customersRepository.getCustomerByEmail(email);
     }
 
     static public Customer getCustomerByUsername(String username) {
-        if (customersUsernames.containsKey(username)) {
-            return customersUsernames.get(username);
-        }
-        return null;
+        return customersRepository.getCustomerByUsername(username);
     }
-
 
 }
