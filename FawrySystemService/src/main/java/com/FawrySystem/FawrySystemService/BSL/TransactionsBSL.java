c6ff3b4e8a2a45804c827e0a363c461a3414ad78
fadiakhaled <110059717+fadiakhaled@users.noreply.github.com>
2022-12-28 01:59:51 +0200

    Update TransactionsBSL.java

--- FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/BSL/TransactionsBSL.java
+++ FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/BSL/TransactionsBSL.java
@@ -1,50 +1,35 @@
 package com.FawrySystem.FawrySystemService.BSL;
 
 import com.FawrySystem.FawrySystemService.models.Transactions.Transaction;
+import com.FawrySystem.FawrySystemService.repositories.TransactionRepository;
 
 import java.util.HashMap;
 
 public class TransactionsBSL {
-    // maps of transactions and refund requests
-    private static HashMap<Integer, Transaction> refunds = new HashMap<Integer, Transaction>();
-    private static HashMap<Integer, Transaction> Transactions = new HashMap<Integer, Transaction>();
+    static TransactionRepository transactionRepository = new TransactionRepository();
 
     public static HashMap<Integer, Transaction> getRefunds() {
-        return refunds;
-    }
-    public static HashMap<Integer, Transaction> getTransactions() {
-        return Transactions;
+        return transactionRepository.getRefunds();
     }
 
-    // add transaction to transactions map
-    public void addTransaction(Transaction T)
-    {
-        Transactions.put(T.getTrans_ID(),T);
+    public void addTransaction(Transaction transaction) {
+        transactionRepository.addTransaction(transaction);
     }
 
-    //find transaction by ID
-    public Transaction findTransaction(int id)
-    {
-        return Transactions.get(id);
+    public Transaction findTransaction(int id) {
+        return transactionRepository.findTransaction(id);
     }
 
-    // find refund request by transaction ID
-    public Transaction findRefund(int id)
-    {
-        return refunds.get(id);
+    public Transaction findRefund(int id) {
+        return transactionRepository.findRefund(id);
     }
 
-    // remove transaction from refund requests map
-    public void removeRefundRequest(int id)
-    {
-        if(refunds.containsKey(id)) {
-            refunds.remove(id);
-        }
+    public void removeRefundRequest(int id) {
+        transactionRepository.removeRefundRequest(id);
     }
-    // add transaction to refund requests map
-    public void requestRefund(Transaction T)
-    {
-        refunds.put(T.getTrans_ID(),T);
+
+    public void requestRefund(Transaction transaction) {
+        transactionRepository.requestRefund(transaction);
     }
 
 }
