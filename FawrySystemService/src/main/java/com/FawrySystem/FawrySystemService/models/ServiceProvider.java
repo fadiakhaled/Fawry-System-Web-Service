2b8e75a06a480608ae5c71ddef79b900bc5a9749
fadiakhaled <111294714+habiba-menshawy@users.noreply.github.com>
2022-12-29 22:54:43 +0200

    Update ServiceProvider.java

--- FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/models/ServiceProvider.java
+++ FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/models/ServiceProvider.java
@@ -1,11 +1,8 @@
 package com.FawrySystem.FawrySystemService.models;
 
-import com.FawrySystem.FawrySystemService.models.Transactions.Transaction;
-import com.FawrySystem.FawrySystemService.models.payment.PaymentStrategy;
-
 public abstract class ServiceProvider {
     protected String name;
-
+    public abstract void sendTransactionInformation ();
     public abstract void updateDiscount(Float d);
     public abstract Float getDiscount();
 
