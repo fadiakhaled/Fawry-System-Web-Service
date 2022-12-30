49d5a3f93b8135138c00c83d2a66d15bdae69005
fadiakhaled <118079766+Habiba554@users.noreply.github.com>
2022-12-30 22:04:04 +0200

    Search service API

--- FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/serviceProviderPackage/BSL/BasicBSL.java
+++ FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/serviceProviderPackage/BSL/BasicBSL.java
@@ -4,12 +4,15 @@ import com.FawrySystem.FawrySystemService.formsPackage.forms.DonationsForm;
 import com.FawrySystem.FawrySystemService.formsPackage.forms.LandlineForm;
 import com.FawrySystem.FawrySystemService.serviceProviderPackage.DonationsSPs.DonationSP;
 import com.FawrySystem.FawrySystemService.serviceProviderPackage.LandlineSPs.LandlineSP;
+import com.FawrySystem.FawrySystemService.serviceProviderPackage.ServiceProvidersRepository;
 import com.FawrySystem.FawrySystemService.transactionsPackage.models.Transaction;
 import com.FawrySystem.FawrySystemService.transactionsPackage.repository.TransactionRepository;
 
+import java.util.Vector;
+
 public class BasicBSL {
 
-    public Transaction invokeDonationsSP (DonationsForm form) {
+    public Transaction invokeDonationsSP(DonationsForm form) {
         DonationSP donationSP = new DonationSP();
         boolean status = donationSP.passForm(form);
         if (status) {
@@ -20,7 +23,7 @@ public class BasicBSL {
         return null;
     }
 
-    public Transaction invokeLandlineSP (LandlineForm form) {
+    public Transaction invokeLandlineSP(LandlineForm form) {
         LandlineSP landlineSP = new LandlineSP();
         boolean status = landlineSP.passForm(form);
         if (status) {
@@ -30,5 +33,18 @@ public class BasicBSL {
         }
         return null;
     }
+
+    public Vector<String> searchSP(String spname) {
+        ServiceProvidersRepository repository = new ServiceProvidersRepository();
+        Vector<String> available = repository.getAvailableServiceProviders();
+        Vector<String> result = new Vector<>();
+        for (String name : available) {
+            if (name.toLowerCase().contains(spname.toLowerCase())) {
+                result.add(name);
+            }
+        }
+        return result;
+    }
+
 
 }
