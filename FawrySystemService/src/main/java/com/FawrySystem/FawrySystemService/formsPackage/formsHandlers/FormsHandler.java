5a8184ec336099c9ff7a34f1314dc3fd8ae4d449
fadiakhaled <118079766+Habiba554@users.noreply.github.com>
2022-12-30 21:42:42 +0200

    Update FormsHandler.java

--- FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/formsPackage/formsHandlers/FormsHandler.java
+++ FawrySystemService/src/main/java/com/FawrySystem/FawrySystemService/formsPackage/formsHandlers/FormsHandler.java
@@ -13,4 +13,19 @@ public abstract class FormsHandler {
     Float amount;
     String paymentType;
 
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
 }
